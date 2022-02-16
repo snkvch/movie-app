dc74dd131a9550800ef19df75f795bc342a9e1dc
snkvch <julia.sankevich8@gmail.com>
2022-02-16 16:34:12 +0300

    Add header component with a search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
 
 import Header from 'components/Header/Header';
 import { HomePage } from 'pages/HomePage';
@@ -7,18 +8,20 @@ import { MovieDetails } from 'pages/MovieDetails';
 import { Wishlist } from 'pages/Wishlist';
 
 import './App.css';
+import theme from 'theme';
+
 import { PATHS } from './utils/paths';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
       <Routes>
         <Route path={PATHS.homePage} element={<HomePage />} />
         <Route path={PATHS.movieDetails} element={<MovieDetails />} />
         <Route path={PATHS.wishlist} element={<Wishlist />} />
       </Routes>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { Search, SearchIconWrapper, StyledInputBase } from './SearchBar.styles';
+
+const SEARCHBAR_PLACEHOLDER = 'Search...';
+
+function SearchBar() {
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase placeholder={SEARCHBAR_PLACEHOLDER} />
+    </Search>
+  );
+}
+
+export default React.memo(SearchBar);
