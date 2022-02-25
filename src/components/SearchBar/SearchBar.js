2da352ae410a5959abd9b53a7fe14152ecb20c97
snkvch <78703258+snkvch@users.noreply.github.com>
2022-02-25 14:04:31 +0300

    Add ability to search movies (#5)
    * Add ability to search movies
    
    * Improve miswriting

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import { useDispatch } from 'react-redux';
+
+import { getMovie } from 'redux/movies/actions';
 
 import { Search, SearchIconWrapper, StyledInputBase } from './SearchBar.styles';
 
 const SEARCHBAR_PLACEHOLDER = 'Search...';
 
 function SearchBar() {
+  const [textInput, setTextInput] = useState('');
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(getMovie(textInput));
+  };
+  const handlerChange = (e) => {
+    setTextInput(e.target.value);
+  };
+
   return (
     <Search>
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase placeholder={SEARCHBAR_PLACEHOLDER} />
+      <form onSubmit={submitHandler}>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          value={textInput}
+          placeholder={SEARCHBAR_PLACEHOLDER}
+          onChange={handlerChange}
+        />
+      </form>
     </Search>
   );
 }
